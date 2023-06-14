0d66afe97cbc112ac0b47077e8550a7d7e70b75e
MansaMonk <afrokinn@gmail.com>
2023-06-14 14:52:52 -0500

    Week 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import Portrait from "../assets/Portrait.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                // Image goes here
+                src={Portrait}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Israel Haile</div>
+              <div className="brief_description">
+          {/*Details about you goes here*/}
+          <p>I am a computer science major at Rice University. I am interested
+             in software development, web development, and cybersecurity.</p>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
